1fb47e3413d96feb7cdaa3011b556032164c7b61
Shelby Baker <shelbymbaker91@gmail.com>
2022-03-30 17:49:02 -0600

    connected to the DB

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,20 +1,32 @@
-const getRecords =(req,res) => {
+
+const asyncHandler = require ('express-async-handler')
+
+
+
+
+const getRecords = asyncHandler(async(req,res) => {
     res.status(200).json ({message: 'Get Records'})
-}
+})
 
-const setRecords =(req,res) => {
+const setRecords = asyncHandler(async (req,res) => {
+
+    if(!req.body.text){
+        res.status(400)
+        throw new error ('Please add a text field')
+
+    }
     res.status(200).json ({message: 'Set Records'})
-}
+})
 
-const updateRecords =(req,res) => {
+const updateRecords = asyncHandler(async(req,res) => {
     res.status(200).json ({message: 'update Records'})
-}
+})
 
 
 
-const deleteRecords =(req,res) => {
+const deleteRecords = asyncHandler(async(req,res) => {
     res.status(200).json ({message: 'delete Records'})
-}
+})
 
 module.exports = {
     getRecords,
